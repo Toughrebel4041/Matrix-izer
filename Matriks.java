1d506dae30036e9f4dba7bdb641a9c87dfc5f96a
Toughrebel4041 <raihansultanpb@gmail.com>
2023-12-19 15:55:54 +0700

    added menu and needed constructors

--- Matriks.java
+++ Matriks.java
@@ -2,136 +2,149 @@ import java.util.Scanner;
 
 public class Matriks {
 
-  public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
+    private int[][] resultMatrix;
 
-    // Input for the first matrix
-    System.out.print("Enter the number of rows for the first matrix: ");
-    int rows1 = scanner.nextInt();
-    System.out.print("Enter the number of columns for the first matrix: ");
-    int cols1 = scanner.nextInt();
-    int[][] matrix1 = inputMatrix(rows1, cols1, "first", scanner);
-
-    // Input for the second matrix
-    System.out.print("Enter the number of rows for the second matrix: ");
-    int rows2 = scanner.nextInt();
-    System.out.print("Enter the number of columns for the second matrix: ");
-    int cols2 = scanner.nextInt();
-    int[][] matrix2 = inputMatrix(rows2, cols2, "second", scanner);
-
-    // Display the entered matrices
-    System.out.println("\nFirst Matrix:");
-    printMatrix(matrix1);
-
-    System.out.println("\nSecond Matrix:");
-    printMatrix(matrix2);
-
-    scanner.close();
-  }
-
-  // Function to input elements for a matrix
-  private static int[][] inputMatrix(int rows, int cols, String matrixName, Scanner scanner) {
-    int[][] matrix = new int[rows][cols];
-    System.out.println("Enter the elements for the " + matrixName + " matrix:");
-    for (int i = 0; i < rows; i++) {
-      for (int j = 0; j < cols; j++) {
-        System.out.print("Enter element at position (" + (i + 1) + "," + (j + 1) + "): ");
-        matrix[i][j] = scanner.nextInt();
-      }
+    // Constructor for matrix operations
+    public Matriks(int[][] matrix1, int[][] matrix2) {
+        displayMenu();
+        performOperation(matrix1, matrix2);
     }
-    return matrix;
-  }
 
-  // Function to print a matrix
-  private static void printMatrix(int[][] matrix) {
-    for (int i = 0; i < matrix.length; i++) {
-      for (int j = 0; j < matrix[0].length; j++) {
-        System.out.print(matrix[i][j] + " ");
-      }
-      System.out.println();
+    // Display the menu
+    private void displayMenu() {
+        System.out.println("Matrix Calculator Menu:");
+        System.out.println("1. Addition");
+        System.out.println("2. Subtraction");
+        System.out.println("3. Multiplication");
+        System.out.println("4. Division");
+        System.out.print("Choose an operation (1-4): ");
     }
-  }
 
-  private int[][] resultMatrix;
+    // Perform the selected matrix operation
+    private void performOperation(int[][] matrix1, int[][] matrix2) {
+        Scanner scanner = new Scanner(System.in);
+        int choice = scanner.nextInt();
 
-  // Constructor for addition
-  public Matriks(int[][] matrix1, int[][] matrix2, String operation) {
-        if (operation.equalsIgnoreCase("addition")) {
-            performAddition(matrix1, matrix2);
+        switch (choice) {
+            case 1:
+                performAddition(matrix1, matrix2);
+                break;
+            case 2:
+                performSubtraction(matrix1, matrix2);
+                break;
+            case 3:
+                performMultiplication(matrix1, matrix2);
+                break;
+            case 4:
+                performDivision(matrix1, matrix2);
+                break;
+            default:
+                System.out.println("Invalid choice. Please choose a number between 1 and 4.");
+        }
+
+        scanner.close();
+    }
+
+    // Perform addition
+    private void performAddition(int[][] matrix1, int[][] matrix2) {
+        if (matrix1.length == matrix2.length && matrix1[0].length == matrix2[0].length) {
+            resultMatrix = new int[matrix1.length][matrix1[0].length];
+            for (int i = 0; i < matrix1.length; i++) {
+                for (int j = 0; j < matrix1[0].length; j++) {
+                    resultMatrix[i][j] = matrix1[i][j] + matrix2[i][j];
+                }
+            }
+            System.out.println("Result of Addition:");
+            printMatrix(resultMatrix);
         } else {
-            System.out.println("Invalid operation. Please use 'addition', 'subtraction', 'multiplication', or 'division'.");
+            System.out.println("Matrices must have the same dimensions for addition.");
         }
     }
 
-  // Constructor for subtraction
-  public Matriks(int[][] matrix1, int[][] matrix2, boolean isSubtraction) {
-        if (isSubtraction) {
-            performSubtraction(matrix1, matrix2);
+    // Perform subtraction
+    private void performSubtraction(int[][] matrix1, int[][] matrix2) {
+        if (matrix1.length == matrix2.length && matrix1[0].length == matrix2[0].length) {
+            resultMatrix = new int[matrix1.length][matrix1[0].length];
+            for (int i = 0; i < matrix1.length; i++) {
+                for (int j = 0; j < matrix1[0].length; j++) {
+                    resultMatrix[i][j] = matrix1[i][j] - matrix2[i][j];
+                }
+            }
+            System.out.println("Result of Subtraction:");
+            printMatrix(resultMatrix);
         } else {
-            System.out.println("Invalid operation. Please use 'addition', 'subtraction', 'multiplication', or 'division'.");
+            System.out.println("Matrices must have the same dimensions for subtraction.");
         }
     }
 
-  // Constructor for multiplication
-  public Matriks(int[][] matrix1, int[][] matrix2, int operation) {
-        if (operation == 1) {
-            performMultiplication(matrix1, matrix2);
+    // Perform multiplication
+    private void performMultiplication(int[][] matrix1, int[][] matrix2) {
+        if (matrix1[0].length == matrix2.length) {
+            resultMatrix = new int[matrix1.length][matrix2[0].length];
+            for (int i = 0; i < matrix1.length; i++) {
+                for (int j = 0; j < matrix2[0].length; j++) {
+                    for (int k = 0; k < matrix1[0].length; k++) {
+                        resultMatrix[i][j] += matrix1[i][k] * matrix2[k][j];
+                    }
+                }
+            }
+            System.out.println("Result of Multiplication:");
+            printMatrix(resultMatrix);
         } else {
-            System.out.println("Invalid operation. Please use 'addition', 'subtraction', 'multiplication', or 'division'.");
+            System.out.println("Invalid dimensions for multiplication.");
         }
     }
 
-  // Constructor for division
-  public Matriks(int[][] matrix1, int[][] matrix2, double operation) {
-        if (operation == 1.0) {
-            performDivision(matrix1, matrix2);
-        } else {
-            System.out.println("Invalid operation. Please use 'addition', 'subtraction', 'multiplication', or 'division'.");
+    // Perform division
+    private void performDivision(int[][] matrix1, int[][] matrix2) {
+        System.out.println("Matrix division is not supported in this version.");
+        // You can implement matrix division logic if needed
+    }
+
+    // Method to print a matrix
+    private void printMatrix(int[][] matrix) {
+        for (int[] row : matrix) {
+            for (int value : row) {
+                System.out.print(value + " ");
+            }
+            System.out.println();
         }
     }
 
-  // Perform addition
-  private void performAddition(int[][] matrix1, int[][] matrix2) {
-    // Implementation for addition
-    // Store the result in the resultMatrix field
-  }
+    // Method to input elements for a matrix
+    private static int[][] inputMatrix(int rows, int cols, String matrixName, Scanner scanner) {
+        int[][] matrix = new int[rows][cols];
+        System.out.println("Enter the elements for the " + matrixName + " matrix:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print("Enter element at position (" + (i + 1) + "," + (j + 1) + "): ");
+                matrix[i][j] = scanner.nextInt();
+            }
+        }
+        return matrix;
+    }
 
-  // Perform subtraction
-  private void performSubtraction(int[][] matrix1, int[][] matrix2) {
-    // Implementation for subtraction
-    // Store the result in the resultMatrix field
-  }
+    // Main method for testing
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-  // Perform multiplication
-  private void performMultiplication(int[][] matrix1, int[][] matrix2) {
-    // Implementation for multiplication
-    // Store the result in the resultMatrix field
-  }
+        // Input for the first matrix
+        System.out.print("Enter the number of rows for the first matrix: ");
+        int rows1 = scanner.nextInt();
+        System.out.print("Enter the number of columns for the first matrix: ");
+        int cols1 = scanner.nextInt();
+        int[][] matrix1 = inputMatrix(rows1, cols1, "first", scanner);
 
-  // Perform division
-  private void performDivision(int[][] matrix1, int[][] matrix2) {
-    // Implementation for division
-    // Store the result in the resultMatrix field
-  }
+        // Input for the second matrix
+        System.out.print("Enter the number of rows for the second matrix: ");
+        int rows2 = scanner.nextInt();
+        System.out.print("Enter the number of columns for the second matrix: ");
+        int cols2 = scanner.nextInt();
+        int[][] matrix2 = inputMatrix(rows2, cols2, "second", scanner);
 
-  // Method to display the result matrix
-  public void displayResultMatrix() {
-    // Display the resultMatrix
-  }
+        // Create a Matriks object with the input matrices
+        new Matriks(matrix1, matrix2);
 
-  // Example usage in main method
-    int[][] matrix1 = { { 1, 2 }, { 3, 4 } };
-    int[][] matrix2 = { { 5, 6 }, { 7, 8 } };
-
-    // Example usage of addition constructor
-    Matriks additionCalculator = new Matriks(matrix1, matrix2, "addition");
-
-    // Example usage of subtraction constructor
-    Matriks subtractionCalculator = new Matriks(matrix1, matrix2, true);
-
-    // Example usage of multiplication constructor
-    Matriks multiplicationCalculator = new Matriks(matrix1, matrix2, 1);
-
-    // Example usage of division constructor
-    Matriks divisionCalculator = new Matriks(matrix1, matrix2, 1.0);
-  }
+        scanner.close();
+    }
+}
