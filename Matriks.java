9f33211210894f4f4da81cd9ac82cd3102ba0c9f
Toughrebel4041 <raihansultanpb@gmail.com>
2023-12-19 15:54:17 +0700

    updated generator

--- Matriks.java
+++ Matriks.java
@@ -2,45 +2,54 @@ import java.util.Scanner;
 
 public class Matriks {
 
-  public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-    System.out.println("Enter the number of rows for the first matrix:");
-    int rows1 = scanner.nextInt();
+        // Input for the first matrix
+        System.out.print("Enter the number of rows for the first matrix: ");
+        int rows1 = scanner.nextInt();
+        System.out.print("Enter the number of columns for the first matrix: ");
+        int cols1 = scanner.nextInt();
+        int[][] matrix1 = inputMatrix(rows1, cols1, "first", scanner);
 
-    System.out.println("Enter the number of columns for the first matrix:");
-    int columns1 = scanner.nextInt();
+        // Input for the second matrix
+        System.out.print("Enter the number of rows for the second matrix: ");
+        int rows2 = scanner.nextInt();
+        System.out.print("Enter the number of columns for the second matrix: ");
+        int cols2 = scanner.nextInt();
+        int[][] matrix2 = inputMatrix(rows2, cols2, "second", scanner);
 
-    int[][] matrix1 = new int[rows1][columns1];
+        // Display the entered matrices
+        System.out.println("\nFirst Matrix:");
+        printMatrix(matrix1);
 
-    // Read user input for the first matrix
-    for (int row = 0; row < rows1; row++) {
-      for (int col = 0; col < columns1; col++) {
-        System.out.print("Enter element (" + (row + 1) + ", " + (col + 1) + ") (or 'x' for missing data): ");
-        String input = scanner.nextLine();
-        if (input.equals("x")) {
-          matrix1[row][col] = -1; // set a sentinel value for missing data
-        } else {
-          matrix1[row][col] = Integer.parseInt(input);
-        }
-      }
+        System.out.println("\nSecond Matrix:");
+        printMatrix(matrix2);
+
+        scanner.close();
     }
 
-    // (Repeat the above process for the second matrix if needed)
-
-    // Print the first matrix
-    System.out.println("\nFirst Matrix:");
-    for (int[] row : matrix1) {
-      for (int element : row) {
-        if (element == -1) {
-          System.out.print("| x |");
-        } else {
-          System.out.print("| " + element + " |");
+    // Function to input elements for a matrix
+    private static int[][] inputMatrix(int rows, int cols, String matrixName, Scanner scanner) {
+        int[][] matrix = new int[rows][cols];
+        System.out.println("Enter the elements for the " + matrixName + " matrix:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print("Enter element at position (" + (i + 1) + "," + (j + 1) + "): ");
+                matrix[i][j] = scanner.nextInt();
+            }
         }
-      }
-      System.out.println();
+        return matrix;
     }
 
-    scanner.close();
-  }
+    // Function to print a matrix
+    private static void printMatrix(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    
 }
